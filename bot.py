d92aae2e0bb1bf59f3d93df92a3dc35a18e6eb35
Timothy Best <superspring@gmail.com>
2013-01-29 14:12:38 +1300

    Adding user superspring, removing from #catalyst, adding to

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ protocols = []
 
 admin_nick = [ 'aquaman',
                'heytrav',
+               'superspring',
                'curry-overlord',
              ]
 admin_commands = [ 'curry',
@@ -116,6 +117,7 @@ class BotFactory(protocol.ClientFactory):
         print "Connection failed. Reason: %s" % reason
 
 if __name__ == "__main__":
-    reactor.connectTCP('irc.wgtn.cat-it.co.nz', 6667, BotFactory('#catalyst'))
+#    reactor.connectTCP('irc.wgtn.cat-it.co.nz', 6667, BotFactory('#catalyst'))
+    reactor.connectTCP('irc.freenode.org', 6667, BotFactory('##catalystlunch'))
     reactor.connectTCP('irc.freenode.org', 6667, BotFactory('#catalystlunch'))
     reactor.run()
